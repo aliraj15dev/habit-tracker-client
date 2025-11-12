8a0f588f435b562ef34c6592d7b6ce99d5554393
AliRaj <aliraj15dev@gmail.com>
2025-11-12 20:14:03 +0600

    made responsive public habits page

--- src/pages/PublicHabits.jsx
+++ src/pages/PublicHabits.jsx
@@ -5,7 +5,7 @@ import PublicHabit from "./PublicHabit";
 const PublicHabits = () => {
   const [publicHabits, setPublicHabits] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedCategory, setSelectedCategory] = useState("AllCategory");
 
   useEffect(() => {
     axios.get("http://localhost:3000/userHabits")
@@ -14,26 +14,27 @@ const PublicHabits = () => {
   }, []);
 
   const filteredHabits = publicHabits.filter(habit => {
-    const categoryMatch = selectedCategory === "All" || habit.category === selectedCategory;
+    const categoryMatch = selectedCategory === "AllCategory" || habit.category === selectedCategory;
     const searchMatch = habit.habitTitle.toLowerCase().includes(searchTerm.toLowerCase());
     return categoryMatch && searchMatch;
   });
 
   return (
     <div className="max-w-9/10 mx-auto mt-10">
-      <div className="flex items-center justify-between mb-5">
-        <h2 className="text-4xl font-semibold">
+      <div className="flex flex-col sm:flex-row md:items-center justify-between mb-5">
+        <h2 className="text-3xl font-semibold">
           All Public Habits <span className="text-xl">({filteredHabits.length})</span>
         </h2>
 
-        <input
+        <div className="space-x-5">
+          <input
           type="text"
           placeholder="Search habits..."
           value={searchTerm}
           onChange={e => setSearchTerm(e.target.value)}
           className="border p-2 rounded-lg"
         />
-        
+
         <select
           value={selectedCategory}
           onChange={e => setSelectedCategory(e.target.value)}
@@ -46,9 +47,10 @@ const PublicHabits = () => {
           <option value="Work">Work</option>
           <option value="Study">Study</option>
         </select>
+        </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-5">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {filteredHabits.map((habit) => (
           <PublicHabit key={habit._id} habit={habit} />
         ))}
